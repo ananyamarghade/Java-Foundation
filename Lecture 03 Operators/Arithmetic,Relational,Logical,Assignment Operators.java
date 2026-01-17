70ebf7ea94eb9a42461bed2e9d5c2090550c1223
ananyamarghade <ananyamarghade@gmail.com>
2026-01-17 21:36:52 +0530

    Rename Arithmetic Operators.java to include all operator types

--- Lecture 03 Operators/Arithmetic,Relational,Logical,Assignment Operators.java
+++ Lecture 03 Operators/Arithmetic,Relational,Logical,Assignment Operators.java
@@ -47,31 +47,5 @@ public class OperatorsInJava {
         System.out.println("c *= 2 -> " + c);
         c /= 2;
         System.out.println("c /= 2 -> " + c);
-
-        // 5. Increment / Decrement
-        int d = 10;
-
-        System.out.println("\nIncrement / Decrement:");
-        System.out.println("++d = " + (++d));
-        System.out.println("d++ = " + (d++));
-        System.out.println("d after d++ = " + d);
-        System.out.println("--d = " + (--d));
-        System.out.println("d-- = " + (d--));
-        System.out.println("d after d-- = " + d);
-
-        // 6. Ternary Operator
-        int max = (a > b) ? a : b;
-        System.out.println("\nTernary Operator:");
-        System.out.println("Max of a and b = " + max);
-
-        // 7. Bitwise Operators
-        int p = 5;  
-        int q = 3; 
-
-        System.out.println("\nBitwise Operators:");
-        System.out.println("p & q = " + (p & q));
-        System.out.println("p | q = " + (p | q));
-        System.out.println("p ^ q = " + (p ^ q));
-        System.out.println("~p = " + (~p));
     }
 }
