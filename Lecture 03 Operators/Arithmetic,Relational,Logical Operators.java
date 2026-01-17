c82cb9fab8333542b1047c045ff0f00f126f3f00
ananyamarghade <ananyamarghade@gmail.com>
2026-01-17 21:38:48 +0530

    Create Arithmetic,Relational,Logical Operators.java

---
+++ Lecture 03 Operators/Arithmetic,Relational,Logical Operators.java
@@ -0,0 +1,37 @@
+public class OperatorsInJava {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        // 1. Arithmetic Operators
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+
+        // 2. Relational Operators
+        System.out.println("\nRelational Operators:");
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+
+    
+        // 3. Logical Operators
+  
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("\nLogical Operators:");
+        System.out.println("x && y : " + (x && y));
+        System.out.println("x || y : " + (x || y));
+        System.out.println("!x     : " + (!x));
+
+       }}
