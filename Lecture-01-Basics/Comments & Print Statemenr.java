d5a453feb135d026dc1dc9368efe853cf981418d
ananyamarghade <ananyamarghade@gmail.com>
2026-01-17 20:05:44 +0530

    Create Comments & Print Statemenr.java

---
+++ Lecture-01-Basics/Comments & Print Statemenr.java
@@ -0,0 +1,11 @@
+public class CommentsExample {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        /*
+         * This is a multi-line comment
+         * It can span across multiple lines
+         */
+        int x = 10; 
+        System.out.println("Value of x: " + x);
+    }
+}
