8606e4cc94a209a34bfcd7edc7b2df8c2d9286f3
ananyamarghade <ananyamarghade@gmail.com>
2026-01-17 20:14:17 +0530

    Add DataTypesDemo class to demonstrate data types

---
+++ Lecture-02-Data Types/Other datatypes.java
@@ -0,0 +1,16 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        float f = 3.14f;           // 4 bytes
+        double d = 3.14159265359;  // 8 bytes
+
+        char c = 'A';              // 2 bytes (Unicode character)
+        boolean flag = true;       // 1 bit
+
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + flag);
+      
+    }
+}
