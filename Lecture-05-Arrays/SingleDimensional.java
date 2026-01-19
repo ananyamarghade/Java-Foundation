af8b7b5169afcafa4a6a103654b5c8877f5f1b8e
ananyamarghade <ananyamarghade@gmail.com>
2026-01-19 22:26:38 +0530

    Create SingleDimensional.java

---
+++ Lecture-05-Arrays/SingleDimensional.java
@@ -0,0 +1,11 @@
+public class ArrayDemo {
+	public static void main(String[] args) {
+		int[] ramanujanNumbers = { 1729, 4104, 13832, 20683, 32832 };
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Printing Ramanujan Numbers: ");
+		for (int i = 0; i < n; i++) {
+			System.out.println(ramanujanNumbers[i]);
+		}
+	}
+}
