728a7cc509c4de1789873d549bd8e2b51e057ae2
ananyamarghade <ananyamarghade@gmail.com>
2026-04-21 09:31:46 +0530

    Create Main.java

---
+++ 1) MINI PROJECT JAVA/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        GraduateStudent s1 = new GraduateStudent(
+                "ABCD",
+                20,
+                "SITNGP2006",
+                2,
+                "A",
+                4,
+                8.9,
+                "DATA SCIENCE",
+                "B.Tech"
+        );
+
+        System.out.println("------ Student Details ------");
+        s1.displayGraduateStudent();
+    }
+}
